70c297daa704b4268adb5eb9792371d1cd6c06b6
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-05-16 16:03:29 +0530

    Exception Day-2

---
+++ RulesForException.java
@@ -0,0 +1,33 @@
+package com.hp.exceptions;
+
+class A {
+	public Object add(String a, String b) {
+		int c = Integer.parseInt(a) + Integer.parseInt(b);
+		System.out.println("Add of two numbers :" + c);
+		return null;
+	}
+
+	public void sub(String a, String b) throws Exception {
+		int c = Integer.parseInt(a) - Integer.parseInt(b);
+		System.out.println("sub of two numbers " + c);
+	}
+}
+
+public class RulesForException extends A {
+	public String add(String a, String b) throws ArithmeticException {
+		int c = Integer.parseInt(a) + Integer.parseInt(b);
+		System.out.println("Addition of two numbers :" + c);
+		return null;
+	}
+
+	public void sub(String a, String b) throws ArithmeticException {
+		int c = Integer.parseInt(a) - Integer.parseInt(b);
+		System.out.println("subtraction of two numbers :" + c);
+	}
+
+	public static void main(String[] args) {
+		RulesForException re = new RulesForException();
+		re.add("12", "13");
+		re.sub("11", "12");
+	}
+}
