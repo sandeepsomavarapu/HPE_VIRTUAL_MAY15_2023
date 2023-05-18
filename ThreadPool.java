a34583165f5215ed0edbfdf432eaab589b8e7ece
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-05-18 15:21:09 +0530

    Thread Pool Examples

---
+++ ThreadPool.java
@@ -0,0 +1,22 @@
+package com.hp.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPool {
+	public static void main(String[] args) {
+		// create the pool
+		ExecutorService service = Executors.newFixedThreadPool(10);
+		// submit tasks for execution
+		for (int i = 0; i < 100; i++) {
+			service.execute(new Task());
+		}
+		System.out.println("Thread name :" + Thread.currentThread().getName());
+	}
+}
+
+class Task implements Runnable {
+	public void run() {
+		System.out.println("Thread name :" + Thread.currentThread().getName());
+	}
+}
