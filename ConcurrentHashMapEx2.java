2745c342985b3c8c31d29d2102238b041d8f30ab
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-05-19 15:18:32 +0530

    Day-5 Concurrent Collections

---
+++ ConcurrentHashMapEx2.java
@@ -0,0 +1,37 @@
+package com.hp.collections;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ConcurrentHashMapEx2 extends Thread {
+	static HashMap chm = new HashMap();
+
+	public void run() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+		System.out.println("child thread got chance");
+		chm.put(40, "welcome to IBM");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		chm.put(10, "welcome");
+		chm.put(20, "hello");
+		chm.put(30, "hai");
+		ConcurrentHashMapEx2 t = new ConcurrentHashMapEx2();
+		t.start();
+		Set s = chm.keySet();
+		Iterator itr = s.iterator();
+		while (itr.hasNext()) {
+			Integer i = (Integer) itr.next();
+			System.out.println("main Thread Iterating...");
+			System.out.println("current key is.. : " + i + " and value :" + chm.get(i));
+			Thread.sleep(1000);
+		}
+		System.out.println(chm);
+	}
+
+}
---
+++ ConcurrentLinkedQueueEx1.java
@@ -0,0 +1,18 @@
+package com.hp.collections;
+
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ConcurrentLinkedQueueEx1 
+{
+public static void main(String[] args) 
+{
+
+	
+	ConcurrentLinkedQueue cm=new ConcurrentLinkedQueue();
+	cm.add("x");
+	cm.add("z");
+	cm.add("a");
+	System.out.println(cm);
+}
+}
